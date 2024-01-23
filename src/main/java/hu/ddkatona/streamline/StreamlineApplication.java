e86d41e47971c16e27159c0434b537aa4c727b8d
ddkatona <ddkatona@gmail.com>
2024-01-23 19:26:30 +0100

    Initial commint

---
+++ src/main/java/hu/ddkatona/streamline/StreamlineApplication.java
@@ -0,0 +1,13 @@
+package hu.ddkatona.streamline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamlineApplication.class, args);
+	}
+
+}
